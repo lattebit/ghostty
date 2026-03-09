3dc5ecd0695adb5993d1876d6e6fee273ae6676b
dqaria <dqaria@gmail.com>
2026-03-09 19:45:24 +1030

    Add iOS C API exports (write_pty_output, is_alternate_screen)
    ghostty_surface_write_pty_output: feed external data to the terminal
    as if from PTY, primary input mechanism on iOS.
    ghostty_surface_is_alternate_screen: detect vim/less/htop for
    scrollback management.

--- include/ghostty.h
+++ include/ghostty.h
@@ -1110,6 +1110,8 @@ bool ghostty_surface_key_is_binding(ghostty_surface_t,
                                     ghostty_binding_flags_e*);
 void ghostty_surface_text(ghostty_surface_t, const char*, uintptr_t);
 void ghostty_surface_preedit(ghostty_surface_t, const char*, uintptr_t);
+void ghostty_surface_write_pty_output(ghostty_surface_t, const char*, uintptr_t);
+bool ghostty_surface_is_alternate_screen(ghostty_surface_t);
 bool ghostty_surface_mouse_captured(ghostty_surface_t);
 bool ghostty_surface_mouse_button(ghostty_surface_t,
                                   ghostty_input_mouse_state_e,
